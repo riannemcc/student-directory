b62f550274c50a14d171a0819f56095f12a7293c
Rianne McCartney <riannemccartney@hotmail.com>
2019-06-17 19:51:04 +0100

    exercise6 complete

--- directory.rb
+++ directory.rb
@@ -18,16 +18,16 @@ def input_students
 end
 
   def print_header
-  puts "The students of Villains Academy"
-  puts "____________"
+  puts "The students of Villains Academy".center(100)
+  puts "____________".center(100)
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100,".")
   end
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(100)
 end
 #nothing happens until we call the methods
 students = input_students
