8f42e5daef2f2c48edb0d6a87b324fff496054e2
Rianne McCartney <riannemccartney@hotmail.com>
2019-06-18 15:16:51 +0100

    exercise10 complete

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.strip
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = gets.strip
   end
   #return the array of students
   students
@@ -25,7 +25,6 @@ def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
-end
 end
 
 def print_footer(students)
